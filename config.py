fa436d34e304223fd4acc69e4965aea3cc463b58
達南佳亞 <93440107+TEAM-DLK@users.noreply.github.com>
2025-02-10 15:29:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 API_ID = "your_api_id"
 API_HASH = "your_api_hash"
 BOT_TOKEN = "your_bot_token"
-COOKIES_PATH = "cookies.txt"  # Path to cookies file
+COOKIES_PATH = "cookies/cookies.txt"  # Path to cookies file
 CHANNEL_USERNAME = "YourChannelUsername"  # Without @
